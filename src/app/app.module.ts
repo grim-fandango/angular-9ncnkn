b3f0dd5561d708fcf6dc5099682f2dd7ed898889
grim-fandango <jkrobertson@live.com>
2021-07-06 15:14:19 +0100

    Cart added, not working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { CartComponent } from './cart/cart.component';
+import { CartService } from './cart.service';
 
 @NgModule({
   imports: [
@@ -30,7 +31,8 @@ import { CartComponent } from './cart/cart.component';
   ],
   bootstrap: [
     AppComponent
-  ]
+  ],
+  providers: [CartService]
 })
 export class AppModule { }
 
