b623f03b8d47b3edda7ac317fe35b9ccbed960f0
grim-fandango <jkrobertson@live.com>
2021-07-06 16:55:07 +0100

    2 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,7 +18,7 @@ import { CartService } from './cart.service';
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
       { path: 'products/:productId', component: ProductDetailsComponent },
-      { path: 'cart', component: CartComponent }
+      { path: 'cart', component: CartComponent },
     ])
   ],
   declarations: [
